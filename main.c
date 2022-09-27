abd31fd56fcce356c6e3eb94e0e79d84d9116b90
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2022-09-27 19:19:55 +0900

    number culc

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 int main(){
-  printf("こんにちは。私の名前は%sです。\n年齢は%d歳です。\n", "山田太郎", 20 + 5);
-  printf("イニシャルは、%cです。\n", 'Y');
-  printf("%f + %f = %f\n", 1.2, 2.7, 1.2 + 2.7);
+  printf("%d + %d + %d = %d\n", 1, 2, 3, 1 + 2 + 3);
 }
