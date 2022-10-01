1afb8a96de98489b1d3a4b9319607e715644f287
t0yohei <inuneko690121@gmail.co>
2022-10-01 16:37:35 +0900

    rand if

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 int main()
 {
-    int width, heigth;
-    printf("長方形の幅: "); //  キーボードからの入力を促す
-    scanf("%d", &width);    //  キーボードから一文字入力
-    printf("長方形の高さ: ");
-    scanf("%d", &heigth);
-    printf("長方形の面積は %dmです。\n", width * heigth);
-
-    // 長方形の幅： 9   ← キーボードから整数値を入力後、改行
-    // 長方形の高さ： 4   ← キーボードから整数値を入力後、改行
-    // 長方形の面積は、36mです。
+    int n;
+    srand((unsigned)time(NULL));
+    n = rand() % 6 + 1;
+    printf("数値：%d\n", n);
+    if (n >= 3)
+    {
+        printf("3以上です。");
+    }
 }
