8e760ca0a061ee8575e24a5789ad66983d305b5d
t0yohei <inuneko690121@gmail.co>
2022-10-01 16:01:16 +0900

    input culc

--- main.c
+++ main.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
-int main(){
-  printf("%d + %d + %d = %d\n", 1, 2, 3, 1 + 2 + 3);
+
+int main()
+{
+    // int a;                          //  変数a（キーボードからの値を代入)
+    // printf("a=");                   //  キーボードからの入力を促す
+    // scanf("%d", &a);                //  キーボードから一文字入力
+    // printf("aの値は%dです。\n", a); //  aの値を表示
+    // printf("aの値は%dです。\n", a); //  aの値を表示
+
+    int a, b;
+    printf("a=");    //  キーボードからの入力を促す
+    scanf("%d", &a); //  キーボードから一文字入力
+    printf("b=");
+    scanf("%d", &b);
+    printf("a + b = %dです。\n", a + b);
+    printf("a - b = %dです。\n", a - b);
+    printf("a * b = %dです。\n", a * b);
+    printf("a / b = %dです。\n", a / b);
+    printf("a %% b = %dです。\n", a % b);
+
+    // a = 9   ← キーボードから整数値を入力後、改行
+    // b = 4   ← キーボードから整数値を入力後、改行
+    // a + b = 13
+    // a - b = 5
+    // a * b = 36
+    // a / b = 2
+    // a % b = 1
 }
